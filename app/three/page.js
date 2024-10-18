f3523489ddb1674bfd77dc2e7ab6cfd163a30662
Logan Patterson <67162730+LoganTylerPatterson@users.noreply.github.com>
2024-10-17 21:36:28 -0700

    Full office ye

--- app/three/page.js
+++ app/three/page.js
@@ -1,23 +1,24 @@
 'use client'
 import { Canvas, useLoader } from "@react-three/fiber";
-import { ArcadeCabinet } from "./arcade/page";
 import { OrbitControls } from "@react-three/drei";
+import OfficeScene from "@/components/OfficeScene";
 
 export default function Office() {
     return (
         <div className="h-screen w-screen">
             <Canvas
                 shadows
-                className="bg-white"
+                className="bg-black"
                 camera={{
                     position: [7, 7, 7]
                 }}
             >
                 <ambientLight />
                 <pointLight position={[7, 7, 7]}/>
+                <directionalLight/>
                 <OrbitControls />
-                <ArcadeCabinet />
+                <OfficeScene />
             </Canvas>
         </div>
     )
-}
+}         
