0ad2bf9e145443ef751bb3efce5a19854722e95d
Logan Patterson <67162730+LoganTylerPatterson@users.noreply.github.com>
2024-10-18 21:53:38 -0700

    nicer lighting

--- app/three/page.js
+++ app/three/page.js
@@ -1,23 +1,27 @@
 'use client'
 import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stage } from "@react-three/drei";
 import OfficeScene from "@/components/OfficeScene";
+import { Suspense, useRef } from "react";
 
 export default function Office() {
+    const ref = useRef()
     return (
         <div className="h-screen w-screen">
             <Canvas
                 shadows
-                className="bg-black"
-                camera={{
-                    position: [7, 7, 7]
-                }}
+                className="bg-lightblue"
+                dpr={[1, 2]} 
+                camera={{ fov: 50 }}
             >
-                <ambientLight />
-                <pointLight position={[7, 7, 7]}/>
-                <directionalLight/>
-                <OrbitControls />
-                <OfficeScene />
+                <Suspense fallback={null}>
+                    <Stage controls={ref} preset="rembrandt" intensity={1}  environment="city">
+                        false
+                        <OfficeScene />
+                        false
+                    </Stage>
+                </Suspense>
+                <OrbitControls ref={ref}/>
             </Canvas>
         </div>
     )
