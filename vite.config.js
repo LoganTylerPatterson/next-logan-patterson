1aa6539184cc59039f1861b8a38be7a189ba9182
Logan Patterson <67162730+LoganTylerPatterson@users.noreply.github.com>
2025-01-24 20:31:18 -0700

    replace with static site

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import restart from 'vite-plugin-restart'
+import glsl from 'vite-plugin-glsl'
+import { dirname, resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    },
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true, // Add sourcemap
+        rollupOptions: {
+            input: {
+                branches: resolve(__dirname, 'src/pages/branches/index.html')
+            }
+        }
+    },
+    plugins:
+    [
+        restart({ restart: [ '../static/**', ] }), // Restart server on static file change
+        glsl() // Handle shader files
+    ]
+}
